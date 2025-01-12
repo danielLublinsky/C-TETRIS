12834c71cfd29dcf2c9b4e7f253461d300e648cf
Daniel <zzzxxxzz123abc@gmail.com>
2025-01-12 14:52:31 +0200

    using char, bug fixes

--- Main.c
+++ Main.c
@@ -4,35 +4,31 @@
 
 #define BOARD_WIDTH 10
 #define BOARD_HEIGHT 20
-#define MAX_FIGURE_WIDTH 5
-#define MAX_FIGURE_HEIGHT 5
+#define MAX_FIGURE_WIDTH 3
+#define MAX_FIGURE_HEIGHT 3
 
 typedef struct
 {
-    int scoreVal;
-    int **game_board;
+    char **game_board;
     int figure_x, figure_y;
 } game_state_t;
 
-
 //demo
-int active_figure[MAX_FIGURE_HEIGHT][MAX_FIGURE_WIDTH] = {
-    {1, 1, 1, 1, 0},  
-    {1, 0, 0, 0, 0},
-    {1, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0}
+char active_figure[MAX_FIGURE_HEIGHT][MAX_FIGURE_WIDTH] = {
+    {'*', '*', ' '},  
+    {'*', ' ', ' '},
+    {'*', ' ', ' '}
 };
 
 void initialize_board(game_state_t *game_state) {
-    game_state->game_board = (int **)malloc(BOARD_HEIGHT * sizeof(int *));
+    game_state->game_board = (char **)malloc(BOARD_HEIGHT * sizeof(char *));
     for (int i = 0; i < BOARD_HEIGHT; i++) {
-        game_state->game_board[i] = (int *)malloc(BOARD_WIDTH * sizeof(int));
+        game_state->game_board[i] = (char *)malloc(BOARD_WIDTH * sizeof(char));
     }
 
     for (int i = 0; i < BOARD_HEIGHT; i++) {
         for (int j = 0; j < BOARD_WIDTH; j++) {
-            game_state->game_board[i][j] = 0;  
+            game_state->game_board[i][j] = ' ';  
         }
     }
 }
@@ -47,7 +43,7 @@ void free_board(game_state_t *game_state) {
 void print_board(game_state_t *game_state) {
     for (int i = 0; i < BOARD_HEIGHT; i++) {
         for (int j = 0; j < BOARD_WIDTH; j++) {
-            printf("%d ", game_state->game_board[i][j]);
+            printf("%c ", game_state->game_board[i][j]);
         }
         printf("\n");
     }
@@ -55,15 +51,15 @@ void print_board(game_state_t *game_state) {
 
 void display_figure(game_state_t *game_state){
     for (int i = 0; i < MAX_FIGURE_HEIGHT; i++) {
-        for (int y = 0; y < MAX_FIGURE_WIDTH; y++ ) {
-            int x_pos = game_state->figure_x + i;
-            int y_pos = game_state->figure_y + y;
+        for (int j = 0; j < MAX_FIGURE_WIDTH; j++) {
+            int y_pos = game_state->figure_y + i; 
+            int x_pos = game_state->figure_x + j;
 
-            if (x_pos >= 0 && x_pos < BOARD_HEIGHT && y_pos >= 0 && y_pos < BOARD_WIDTH) {
-                if (game_state->game_board[x_pos][y_pos] == 0 && active_figure[i][y] != 0) {
-                    game_state->game_board[x_pos][y_pos] = active_figure[i][y];
+            if (y_pos >= 0 && y_pos < BOARD_HEIGHT && x_pos >= 0 && x_pos < BOARD_WIDTH) {
+                if (game_state->game_board[y_pos][x_pos] == ' ') {
+                    game_state->game_board[y_pos][x_pos] = active_figure[i][j];  // Correct figure placement
                 }
-                else if (game_state->game_board[x_pos][y_pos] != 0 && active_figure[i][y] != 0) {
+                else if (game_state->game_board[y_pos][x_pos] == '#' && active_figure[i][j] == '*') {
                     printf("Collision detected\n");
                     free_board(game_state);
                     exit(1);
@@ -73,33 +69,48 @@ void display_figure(game_state_t *game_state){
                 free_board(game_state);
                 exit(1);
             }
-
         }
     }
 }
+
+void clear_figure(game_state_t *game_state) {
+     for (int i = 0; i < MAX_FIGURE_HEIGHT; i++) {
+        for (int j = 0; j < MAX_FIGURE_WIDTH; j++) {
+            int y_pos = game_state->figure_y + i; 
+            int x_pos = game_state->figure_x + j;
+            
+            if (y_pos >= 0 && y_pos < BOARD_HEIGHT && x_pos >= 0 && x_pos < BOARD_WIDTH) {
+                if(game_state->game_board[y_pos][x_pos] != '#') {
+                    game_state->game_board[y_pos][x_pos] = ' '; 
+                }
+            }
+        }
+    }
+}
+
 void update_board(game_state_t *game_state) {
-    //system("clear");
+    system("clear");
     display_figure(game_state);
     print_board(game_state);
 }
 
 
-void move_figure(game_state_t *game_state) {
+void drop_figure(game_state_t *game_state) {
+    clear_figure(game_state);
     game_state->figure_y++;
     update_board(game_state);
 }
 
 int main() {
     game_state_t game_state;
-    game_state.scoreVal = 0;
-    game_state.figure_x = 0;
-    game_state.figure_y = 0;
+    game_state.figure_x = 2;
+    game_state.figure_y = 2;
 
     initialize_board(&game_state);
 
     while (1)
     {
-        update_board(&game_state);
+        drop_figure(&game_state);
         getchar();
     }
     
