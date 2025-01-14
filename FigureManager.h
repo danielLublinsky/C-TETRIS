efce070cde812a37ea3be8509dd8aed239a56d4e
Daniel <zzzxxxzz123abc@gmail.com>
2025-01-14 18:55:22 +0200

    refactor + stuff

--- FigureManager.h
+++ FigureManager.h
@@ -7,27 +7,40 @@
 #define MAX_FIGURE_WIDTH 3
 #define MAX_FIGURE_HEIGHT 3
 
+typedef struct {
+    char **figure_grid;
+    int hight, width;
+} active_figure_t;
+
 const char figure_templates[3][MAX_FIGURE_HEIGHT][MAX_FIGURE_WIDTH] = {
     {
-        {'*', '*', ' '},  
+        {'*', '*', ' '},
         {'*', ' ', ' '},
         {'*', ' ', ' '}
     },
     {
-        {'*', '*', '*'},  
+        {'*', '*', '*'},
         {' ', '*', ' '},
         {' ', '*', ' '}
     },
     {
-        {'*', '*', '*'},  
+        {'*', '*', '*'},
         {' ', ' ', ' '},
         {' ', ' ', ' '}
     }
 };
 
+void initialize_figure(active_figure_t *active_figure) {
+    active_figure->figure_grid = (char **)malloc(MAX_FIGURE_HEIGHT * sizeof(char *));
+    for (int i = 0; i < MAX_FIGURE_HEIGHT; i++) {
+        active_figure->figure_grid[i] = (char *)malloc(MAX_FIGURE_WIDTH * sizeof(char));
+    }
+    generate_new_figure(active_figure->figure_grid);
+    get_figure_size(active_figure);
+}
+
 void generate_new_figure(char **figure) {
-    srand(time(NULL));
-    int template_index = rand() % (2 + 1); 
+    int template_index = rand() % (2 + 1);
     for (int i = 0; i < MAX_FIGURE_HEIGHT; i++) {
         for (int j = 0; j < MAX_FIGURE_WIDTH; j++) {
             figure[i][j] = figure_templates[template_index][i][j];
@@ -49,6 +62,20 @@ void rotate_figure(char **figure) {
             figure[i][j] = rotated_figure[i][j];
         }
     }
+}
+
+void get_figure_size(active_figure_t *active_figure) {
+    int temp_hight = -1, temp_width = -1;
+    for (int i = 0; i < MAX_FIGURE_HEIGHT; i++) {
+        for (int j = 0; j < MAX_FIGURE_WIDTH; j++) {
+            if (active_figure->figure_grid[i][j] == '*') {
+                if (i > temp_hight) temp_hight = i;
+                if (j > temp_width) temp_width = j;
+            }
+        }
+    }
+    active_figure->hight = temp_hight + 1;
+    active_figure->width = temp_width + 1;
 }
 
 #endif
--- Main.c
+++ Main.c
@@ -4,8 +4,8 @@
 #include <unistd.h>
 #include <termios.h>
 #include <sys/select.h>
+#include <time.h>
 #include "FigureManager.h"
-
 
 #define BOARD_WIDTH 13
 #define BOARD_HEIGHT 25
@@ -13,38 +13,25 @@
 
 typedef struct {
     char **game_board;
-    char **active_figure;
-    int figure_x, figure_y;
+    active_figure_t *active_figure;
+    int figure_posX, figure_posY;
 } game_state_t;
-
 
 void initialize_board(game_state_t *game_state) {
     game_state->game_board = (char **)malloc(BOARD_HEIGHT * sizeof(char *));
     for (int i = 0; i < BOARD_HEIGHT; i++) {
         game_state->game_board[i] = (char *)malloc(BOARD_WIDTH * sizeof(char));
-    }
-
-    for (int i = 0; i < BOARD_HEIGHT; i++) {
         for (int j = 0; j < BOARD_WIDTH; j++) {
-            game_state->game_board[i][j] = ' ';  
+            game_state->game_board[i][j] = ' ';
         }
     }
 }
 
-void initialize_figure(game_state_t *game_state) {
-    game_state->active_figure = (char **)malloc(MAX_FIGURE_HEIGHT * sizeof(char *));
+void free_prog(game_state_t *game_state) {
     for (int i = 0; i < MAX_FIGURE_HEIGHT; i++) {
-        game_state->active_figure[i] = (char *)malloc(MAX_FIGURE_WIDTH * sizeof(char));
+        free(game_state->active_figure->figure_grid[i]);
     }
-    generate_new_figure(game_state->active_figure);
-}
-
-
-void free_game(game_state_t *game_state) {
-    for (int i = 0; i < MAX_FIGURE_HEIGHT; i++) {
-        free(game_state->active_figure[i]);
-    }
-    free(game_state->active_figure);
+    free(game_state->active_figure->figure_grid);
 
     for (int i = 0; i < BOARD_HEIGHT; i++) {
         free(game_state->game_board[i]);
@@ -61,73 +48,95 @@ void print_board(game_state_t *game_state) {
     }
 }
 
-void display_figure(game_state_t *game_state){
-    for (int i = 0; i < MAX_FIGURE_HEIGHT; i++) {
-        for (int j = 0; j < MAX_FIGURE_WIDTH; j++) {
-            int y_pos = game_state->figure_y + i; 
-            int x_pos = game_state->figure_x + j;
-
-            if (y_pos < 0 || x_pos < 0 ) {
-                printf("Out of bounds\n");
-                free_game(game_state);
-                exit(1);
+void clear_full_rows(game_state_t *game_state) {
+    for (int i = 0; i < BOARD_HEIGHT; i++) {
+        int charCount = 0;
+        for (int j = 0; j < BOARD_WIDTH; j++) {
+            if (game_state->game_board[i][j] != ' ') {
+                charCount++;
             }
-
-            if (y_pos < BOARD_HEIGHT && x_pos < BOARD_WIDTH) {
-                if (game_state->game_board[y_pos][x_pos] == '#' && game_state->active_figure[i][j] == '*') {
-                    clear_figure(game_state);
-                    game_state->figure_y--;
-                    write_figure(game_state);
-                    prep_new_figure(game_state);
-                    return;
-                } else if (y_pos == BOARD_HEIGHT-1 && game_state->active_figure[i][j] == '*') {
-                    write_figure(game_state);
-                    prep_new_figure(game_state);
-                    return;
-                } else if (game_state->game_board[y_pos][x_pos] == ' ') {
-                    game_state->game_board[y_pos][x_pos] = game_state->active_figure[i][j];
+        }
+        if (charCount == BOARD_WIDTH) {
+            for (int y = i; y > 0; y--) {
+                for (int x = 0; x < BOARD_WIDTH; x++) {
+                    game_state->game_board[y][x] = game_state->game_board[y - 1][x];
                 }
+            }
+            
+            for (int x = 0; x < BOARD_WIDTH; x++) {
+                game_state->game_board[0][x] = ' ';
+            }
+        }
+    }
+}
+
+int check_collision(game_state_t *game_state) {
+    for (int i = 0; i < game_state->active_figure->hight; i++) {
+        for (int j = 0; j < game_state->active_figure->width; j++) {
+            int y_pos = game_state->figure_posY + i;
+            int x_pos = game_state->figure_posX + j;
+
+            if (y_pos >= BOARD_HEIGHT || x_pos < 0 || x_pos >= BOARD_WIDTH) {
+                if (game_state->active_figure->figure_grid[i][j] == '*') {
+                    return 1;
+                }
+            } else if (y_pos >= 0 && game_state->active_figure->figure_grid[i][j] == '*' && game_state->game_board[y_pos][x_pos] == '#') {
+                return 1; 
+            }
+        }
+    }
+    return 0;
+}
+
+void display_figure(game_state_t *game_state) {
+    for (int i = 0; i < game_state->active_figure->hight; i++) {
+        for (int j = 0; j < game_state->active_figure->width; j++) {
+            int y_pos = game_state->figure_posY + i;
+            int x_pos = game_state->figure_posX + j;
+
+            if (y_pos >= 0 && y_pos < BOARD_HEIGHT && x_pos >= 0 && x_pos < BOARD_WIDTH && game_state->active_figure->figure_grid[i][j] == '*') {
+                game_state->game_board[y_pos][x_pos] = game_state->active_figure->figure_grid[i][j];
             }
         }
     }
 }
 
 void clear_figure(game_state_t *game_state) {
-     for (int i = 0; i < MAX_FIGURE_HEIGHT; i++) {
-        for (int j = 0; j < MAX_FIGURE_WIDTH; j++) {
-            int y_pos = game_state->figure_y + i; 
-            int x_pos = game_state->figure_x + j;
-            
+    for (int i = 0; i < game_state->active_figure->hight; i++) {
+        for (int j = 0; j < game_state->active_figure->width; j++) {
+            int y_pos = game_state->figure_posY + i;
+            int x_pos = game_state->figure_posX + j;
+
             if (y_pos >= 0 && y_pos < BOARD_HEIGHT && x_pos >= 0 && x_pos < BOARD_WIDTH) {
-                if(game_state->game_board[y_pos][x_pos] != '#') {
-                    game_state->game_board[y_pos][x_pos] = ' '; 
+                if (game_state->game_board[y_pos][x_pos] != '#') {
+                    game_state->game_board[y_pos][x_pos] = ' ';
                 }
             }
         }
     }
 }
 
-void write_figure(game_state_t *game_state){
-     for (int i = 0; i < MAX_FIGURE_HEIGHT; i++) {
-        for (int j = 0; j < MAX_FIGURE_WIDTH; j++) {
-            int y_pos = game_state->figure_y + i; 
-            int x_pos = game_state->figure_x + j;
-            
+void write_figure(game_state_t *game_state) {
+    for (int i = 0; i < game_state->active_figure->hight; i++) {
+        for (int j = 0; j < game_state->active_figure->width; j++) {
+            int y_pos = game_state->figure_posY + i;
+            int x_pos = game_state->figure_posX + j;
+
             if (y_pos >= 0 && y_pos < BOARD_HEIGHT && x_pos >= 0 && x_pos < BOARD_WIDTH) {
-                if(game_state->active_figure[i][j] == '*') {
-                    game_state->game_board[y_pos][x_pos] = '#'; 
+                if (game_state->active_figure->figure_grid[i][j] == '*') {
+                    game_state->game_board[y_pos][x_pos] = '#';
                 }
             }
         }
     }
 }
 
-void prep_new_figure(game_state_t *game_state){
-    generate_new_figure(game_state->active_figure);
-    game_state->figure_y = 0;
-    game_state->figure_x = rand() % (5 + 1); 
+void prep_new_figure(game_state_t *game_state) {
+    generate_new_figure(game_state->active_figure->figure_grid);
+    get_figure_size(game_state->active_figure);
+    game_state->figure_posY = 0;
+    game_state->figure_posX = rand() % (BOARD_WIDTH - game_state->active_figure->width + 1);
 }
-
 
 void update_board(game_state_t *game_state) {
     system("clear");
@@ -136,25 +145,53 @@ void update_board(game_state_t *game_state) {
 }
 
 void drop_figure(game_state_t *game_state) {
-        clear_figure(game_state);
-        game_state->figure_y++;
+    clear_figure(game_state);
+    game_state->figure_posY++;
+
+    if (check_collision(game_state)) {
+        game_state->figure_posY--;
+        write_figure(game_state);
+        clear_full_rows(game_state);
+        prep_new_figure(game_state);
+    } else {
         update_board(game_state);
+    }
 }
 
 void on_left_arrow(game_state_t *game_state) {
-   clear_figure(game_state);
-   if (game_state->figure_x > 0){game_state->figure_x--;}
-   update_board(game_state);
+    clear_figure(game_state);
+    game_state->figure_posX--;
+
+    if (check_collision(game_state)) {
+        game_state->figure_posX++;
+    }
+
+    update_board(game_state);
 }
 
 void on_right_arrow(game_state_t *game_state) {
     clear_figure(game_state);
-    if (game_state->figure_x < BOARD_WIDTH){game_state->figure_x++;}
+    game_state->figure_posX++;
+
+    if (check_collision(game_state)) {
+        game_state->figure_posX--;
+    }
+
     update_board(game_state);
 }
+
 void on_space_press(game_state_t *game_state) {
     clear_figure(game_state);
-    rotate_figure(game_state->active_figure);
+    rotate_figure(game_state->active_figure->figure_grid);
+    get_figure_size(game_state->active_figure);
+
+    if (check_collision(game_state)) {
+        rotate_figure(game_state->active_figure->figure_grid);
+        rotate_figure(game_state->active_figure->figure_grid);
+        rotate_figure(game_state->active_figure->figure_grid);
+        get_figure_size(game_state->active_figure);
+    }
+
     update_board(game_state);
 }
 
@@ -162,8 +199,8 @@ int get_keypress_nonblocking() {
     struct timeval timeout;
     fd_set readfds;
 
-    timeout.tv_sec = 0; 
-    timeout.tv_usec = 0; 
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 0;
 
     FD_ZERO(&readfds);
     FD_SET(STDIN_FILENO, &readfds);
@@ -180,27 +217,30 @@ int get_keypress_nonblocking() {
 void set_terminal_mode() {
     struct termios new_settings;
     tcgetattr(STDIN_FILENO, &new_settings);
-    new_settings.c_lflag &= ~ICANON;          
-    new_settings.c_lflag &= ~ECHO;            
-    tcsetattr(STDIN_FILENO, TCSANOW, &new_settings);  
+    new_settings.c_lflag &= ~ICANON;
+    new_settings.c_lflag &= ~ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_settings);
 }
 
 void restore_terminal_mode() {
     struct termios old_settings;
-    tcgetattr(STDIN_FILENO, &old_settings);  
-    old_settings.c_lflag |= ICANON;           
-    old_settings.c_lflag |= ECHO;          
-    tcsetattr(STDIN_FILENO, TCSANOW, &old_settings); 
+    tcgetattr(STDIN_FILENO, &old_settings);
+    old_settings.c_lflag |= ICANON;
+    old_settings.c_lflag |= ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &old_settings);
 }
 
 int main() {
     game_state_t game_state;
-    game_state.figure_x = 3;
-    game_state.figure_y = 0;
-    initialize_figure(&game_state);
+    game_state.figure_posX = 3;
+    game_state.figure_posY = 0;
+
+    game_state.active_figure = malloc(sizeof(active_figure_t));
+    initialize_figure(game_state.active_figure);
     initialize_board(&game_state);
-    
+
     set_terminal_mode();
+    srand(time(NULL));
 
     char ch;
     int drop_counter = 0;
@@ -208,30 +248,30 @@ int main() {
     while (1) {
         ch = get_keypress_nonblocking();
 
-        if (ch) { 
+        if (ch) {
             switch (ch) {
-                case 67: 
+                case 67:
                     on_right_arrow(&game_state);
                     break;
-                case 68:  
+                case 68:
                     on_left_arrow(&game_state);
                     break;
-                case 32: 
+                case 32:
                     on_space_press(&game_state);
                     break;
             }
         }
 
-        if (drop_counter >= PROG_TICK / 1000) { 
+        if (drop_counter >= PROG_TICK / 1000) {
             drop_figure(&game_state);
-            drop_counter = 0; 
+            drop_counter = 0;
         }
 
         usleep(1000);
         drop_counter++;
     }
 
-    free_game(&game_state);
+    free_prog(&game_state);
     restore_terminal_mode();
     return 0;
 }
